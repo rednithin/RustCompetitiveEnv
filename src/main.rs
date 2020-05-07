9a354ced0a8c5d028a12e2868db0527838dfaccc
rednithin <reddy.nithinpg@live.com>
2020-05-08 00:38:03 +0530

    Allowing unused imports.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+#![allow(unused_imports)]
+
 mod competitive;
-use std::collections::HashMap;
+use competitive::*;
+
+use std::collections::{
+    BTreeMap,   // Sorted Map
+    BTreeSet,   // Sorted Set
+    BinaryHeap, // Max Heap Implementation
+    HashMap,    // Map
+    HashSet,    // Set
+    LinkedList, // Linked List not to be used anytime
+    VecDeque,   // Deque
+};
+
+use std::cmp;
 
 fn main() {
     // let mut lcg = rand::LCG::new(1664525, 1013904223, 1 << 47, Some(0)); // Java's Constants
-    // let mut lcg = competitive::rand::LCG::new(6364136223846793005, 1, 1 << 63, None); // MMIX Donald Knuth
-    // let mut lcg = competitive::rand::XoShiro256SS::new([123414, 316241, 761241, 91]); // MMIX Donald Knuth
-    let mut lcg = competitive::rand::XoShiro256SS::new(Some(10)); // MMIX Donald Knuth
+    // let mut lcg = rand::LCG::new(6364136223846793005, 1, 1 << 63, None); // MMIX Donald Knuth
+    // let mut lcg = rand::XoShiro256SS::new(None); // XoShiro256SS
+
+    let mut lcg = rand::XoShiro256SS::new(Some(10)); // XoShiro256SS
 
     let mut map: HashMap<u64, u64> = HashMap::new();
 
